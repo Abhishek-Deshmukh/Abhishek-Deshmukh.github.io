c2d21ef547e712fa83405509652ae6ae216213fc
shriman keshri <shrimansoft@hotmail.com>
2019-02-09 19:08:39 +0530

    the manu will colapes after clecking outside

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,11 @@
 
 
+$(function(){
+    $("#navbarToggle").blur(function (event){
+    
+        var screenWidth=window.innerWidth;
+    if(screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');}});});
   /*-------------------------------------------------------------------------------
     PRE LOADER
   -------------------------------------------------------------------------------*/

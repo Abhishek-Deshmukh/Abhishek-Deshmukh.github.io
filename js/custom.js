166cbe1e30b7ded022ef2fff698917903e2f67d9
shriman keshri <shrimansoft@hotmail.com>
2019-03-17 17:40:43 +0530

    hello fainds

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,76 @@
+//$.fn.$setMainBody = function (global,Html) {
+//
+//    console.log("chack poing 1");
+//    console.log(global);
+//    console.log(Html)
+//
+//var dc ={};
+//    if(Html == undefined){
+//    Html = "snippers/gallary-snippet.html";
+//    }    
+//
+//
+//var insertHtml = function (selector,html) {
+//    var targetElem= document.querySelector(selector);
+//    targetElem.innerHTML = html;
+//};
+//
+//var showLoding = function(selector) {
+//    var html = ' <div id="lodingLogo"> <div class="sk-spinner sk-spinner-wordpress" align=center"> <span class="sk-inner-circle"  ></span> </div> </div>';
+//    insertHtml(selector,html);
+//};
+//
+//
+//document.addEventListener("DOMContentLoaded",function  (event) {
+//    showLoding("#main-content");
+//        $ajaxUtils
+//            .sendGetRequest(Html,
+//                function (request) {
+//                            document.querySelector("#main-content").innerHTML = request.responseText;
+//                            });
+//                });
+//global.$dc=dc;
+//    };
+//
+//$.fn.$setMainBody(window);
+//$.eac
+//
+//
+//$(function() {
+//    $("#MHome").click(function ()
+//        {
+//        $.fn.$setMainBody(window,"snippers/home-snippet.html");
+//        console.log("hello");}
+//);});
+//
+
+$(document).ready(function(){
+
+    var trigger = $('.nav'),
+        container = $('#content');
 
 
+    trigger.on('click',function(){
+    
+        var $this=$(this),
+            target = $this.data('target');
+    
+    
+        container.load('snippers/'+ target + '.php');
+    
+    return false;
+    
+    });
+
+});
+
+$('').click(function(event) {
+    console.log("clicked: " + $(this).data('tagert'));
+});
+
+/*-----------------------------------------------------------------------
+ * ITEM LIST WILL COLLAPS AFTER BLUR
+ * ---------------------------------------------**/ 
 $(function(){
     $("#navbarToggle").blur(function (event){
     
@@ -38,6 +109,7 @@ $(function(){
         });   
         // Animate the scroll to top
       $('.go-top').click(function(event) {
+          console.log(event);
         event.preventDefault();
       $('html, body').animate({scrollTop: 0}, 300);
       })
